8c572130f6d5b62010cd780afc66ea9756674d03
Luthfi Muhaimin <luthfimhmn@gmail.com>
2020-12-18 14:14:45 +0700

    percobaan 6

--- 1.js
+++ 1.js
@@ -59,7 +59,7 @@ function tebak(event) {
 
     console.log(music.src);
 
-    if (music.src == './music/lazy_question.mp3' && response.toLowerCase() == realAnswer[1]) {
+    if (music.src == 'https://luthfimhmn.github.io/music/lazy_question.mp3' && response.toLowerCase() == realAnswer[1]) {
         hasil.setAttribute("class","muncul");
             bersih()
     }
