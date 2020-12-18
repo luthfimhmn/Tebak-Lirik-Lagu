9309e40dea614b7344d3abed04c5b98ee2462c2c
Luthfi Muhaimin <luthfimhmn@gmail.com>
2020-12-18 15:10:16 +0700

    Luthfi Muhaimin

--- 1.js
+++ 1.js
@@ -16,17 +16,17 @@ let arrPictQuestion = [
     "./img/lemontree_question.jpg"
 ]
 
-// let arrMusicAnswer=[
-//     "./music/kopdut_answer.mp3",
-//     "./music/lazy_answer.mp3",
-//     "./music/lemontree_answer.mp3"
-// ]
+let arrMusicAnswer=[
+    "./music/kopdut_answer.mp4",
+    "./music/lazy_answer.mp3",
+    "./music/lemontree_answer.mp3"
+]
 
-// let arrPictAnswer=[
-//     "./img/kopdut_answer.jpg",
-//     "./img/lazy_answer.jpg",
-//     "./img/lemontree_answer.jpg"
-// ]
+let arrPictAnswer=[
+    "./img/kopdut_answer.jpg",
+    "./img/lazy_answer.jpg",
+    "./img/lemontree_answer.jpg"
+]
 
 // let hint = ['... kopi dangdut',
 // '... all',
@@ -57,30 +57,44 @@ function bersih(){
 function tebak(event) {
     let response = answer.value;
 
-    // if (music.src == './music/lazy_question.mp3' && response.toLowerCase() == realAnswer[1]) {
-    //     hasil.setAttribute("class","muncul");
-    //     bersih()
-    // } 
+    if (music.src == 'https://luthfimhmn.github.io/music/kopdut_question.mp3' && response.toLowerCase() == realAnswer[0]) {
+        hasil.innerText = "Anda Benar!!"
+        hasil.setAttribute("class","muncul");
+        bersih()
+        music.src = arrMusicAnswer[0]
+        picture.src = arrPictAnswer[0]
+    } 
     
     /// Belum dapet src-nya. gak masuk kondisi.
 
-    if(response.toLowerCase() == realAnswer[0]){
-            // masukan class hide jadi available
-            hasil.setAttribute("class","muncul");
-            bersih()
-    }else if(response.toLowerCase() == realAnswer[1]){
-            // masukan class hide jadi available
-            hasil.setAttribute("class","muncul");
-            bersih()
-    }else if (response.toLowerCase() == realAnswer[2]){
-            // masukan class hide jadi available
-            hasil.setAttribute("class","muncul");
-            bersih()
-    }else{
-        hasil.innerText = "Kamu masih salah!"
-        hasil.setAttribute("class","muncul");
-        bersih()
-    }
+    // if(response.toLowerCase() == realAnswer[0] &&  arrPictQuestion[0]){
+    //         // masukan class hide jadi available
+    //         hasil.innerText = "Anda Benar!!"
+    //         hasil.setAttribute("class","muncul");
+    //         bersih()
+    //         music.src = arrMusicAnswer[0]
+    //         picture.src = arrPictAnswer[0]
+
+    // }else if(response.toLowerCase() == realAnswer[1] && arrPictQuestion[1]){
+    //         // masukan class hide jadi available
+    //         hasil.innerText = "Anda Benar!!"
+    //         hasil.setAttribute("class","muncul");
+    //         bersih()
+    //         music.src = arrMusicAnswer[1]
+    //         picture.src = arrPictAnswer[1]
+
+    // }else if (response.toLowerCase() == realAnswer[2] && arrPictQuestion[2]){
+    //         // masukan class hide jadi available
+    //         hasil.innerText = "Anda Benar!!"
+    //         hasil.setAttribute("class","muncul");
+    //         bersih()
+    //         music.src = arrMusicAnswer[2]
+    //         picture.src = arrPictAnswer[2]
+    // }else{
+    //     hasil.innerText = "Kamu masih salah!"
+    //     hasil.setAttribute("class","muncul");
+    //     bersih()
+    // }
 }
 
 
